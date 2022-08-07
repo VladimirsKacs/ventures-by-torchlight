e67cc1ec2acc6b53de2b29b7b57661eec32ded95
Vladimirs Kacs <VladimirsKacs@gmail.com>
2022-08-07 19:57:39 +0300

    Initial Commit

---
+++ VentureCore/Adventurer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VentureCore
+{
+    public class Adventurer
+    {
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Constitution { get; set; }
+        public int Hp { get; set; }
+        public int HpMax { get; set; }
+        public int Agility { get; set; }
+        public int CarryCapacity { get; set; }
+        public int Servitude { get; set; }
+        public int Armor { get; set; }
+        public int Xp { get; set; }
+        public int Level { get; set; }
+        public string Name { get; set; }
+        public int Encumbrance { get; set; }
+
+        public List<IItem> Items { get; set; }
+    }
+}
---
+++ VentureCore/IItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VentureCore
+{
+    public interface IItem
+    {
+        int Weight { get; set; }
+        void Eqiup(Adventurer adventurer);
+        void Unequip(Adventurer adventurer);
+
+    }
+}
